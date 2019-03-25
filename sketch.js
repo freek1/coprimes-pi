7326c20ca4576bfa8337047de8e18f34bc507c93
Annick Hens <annickhens@MacBook-Pro-van-Annick.local>
2019-03-25 21:21:31 +0100

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,61 @@
+// Approximating pi using the probabilaty of two random numbers being
+// coprime or not: P(coprime) = 6 / pi^2
+// If two integers, a and b, are coprime, their greatest common deviser
+// equals one: gcd(a, b) = 1; a and b are coprime.
+
+let pi = 0;
+let maximum = 200;
+let iterations = 1;
+let coprimes = 0;
+let update = pi;
+
+function gdc(a, b) {
+	if (a == 0 || b == 0) {
+		return 0;
+	}
+	if (a == b) {
+		return a;
+	}
+	if (a > b) {
+		return gdc(a - b, b);
+	}
+	return gdc(a, b - a);
+}
+
+function coprime(a, b) {
+	if (gdc(a, b) == 1) {
+		coprimes++;
+	}
+}
+
+function setup() {
+	createCanvas(400, 400);
+	stroke(255);
+	fill(255);
+	textSize(34);
+	textAlign(CENTER);
+}
+
+function draw() {
+	frameRate();
+	a = floor(random(1, maximum));
+	b = floor(random(1, maximum));
+
+	coprime(a, b);
+
+	// x = 6/(pi^2)
+	let x = coprimes / iterations;
+	// pi = sqrt(6/x)
+	pi = sqrt(6 / x);
+
+	background(0);
+	text('Pi = '+ PI, width / 2, height / 4);
+	text('Zelf berekend: ', width / 2, height / 2.5);
+	text(pi, width / 2, height / 2);
+
+	if (second() % 2) {
+		update = pi;
+	}
+	text(update, width / 2, 2 * height / 3);
+	iterations++;
+}
